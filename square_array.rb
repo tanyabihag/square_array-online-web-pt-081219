021f77decca802287c066ec168be02a821b614fb
Tanya Bihag <tbihag@gmail.com>
2019-08-20 21:38:30 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  array = []
+  array.square = square_array
+   square_array.each do |number|
+      puts "This is an iterator #{number}"
 end
+
+array = (1, 2, 3)
